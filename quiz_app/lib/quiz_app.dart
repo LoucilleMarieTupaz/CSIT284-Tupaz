425d93cbf8ca6b168f75d94bdfc3bf0c6b03485b
LAB12_USER ON WS19 <L12X16W19@LabHCI.net>
2026-08-26 17:54:53 +0800

    Create quiz homescreen

---
+++ quiz_app/lib/quiz_app.dart
@@ -0,0 +1,25 @@
+// import 'package:flutter/material.dart';
+// class QuizApp extends StatefulWidget {
+//   const QuizApp({super.key});
+//   @override
+//   State<QuizApp> createState() {
+//     return _QuizAppState();
+//   }
+// }
+// class _QuizAppState extends State<QuizApp> {
+//   var currentQuizImage = 'assets/logo.png';
+// }
+// @override
+//   Widget build(context) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Image.asset(width: 200, currentDiceImage),
+//         SizedBox(height: 30),
+//         TextButton(
+//           child: Text(style: TextStyle(fontSize: 28), "Take Quiz"),
+//         ),
+//       ],
+//     );
+//   }
+// }
