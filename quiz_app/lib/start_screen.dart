b053a3e519ea38b9d25f69e0e5ac89c537e3166b
Loucille Tupaz <loucilletupaz@gmail.com>
2026-09-13 02:30:23 +0800

    Extract quiz homescreen from main.dart

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.deepPurple,
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment:MainAxisAlignment.center,
+          children: [
+              Image.asset(
+                'assets/logo.png',
+                width: 180,
+              ),
+              const SizedBox(height: 30), 
+              const Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold, 
+                  color:Colors.white,
+                ),
+              ),
+              const SizedBox(height:30),
+              ElevatedButton(onPressed: () {}, 
+                child: const Text(
+                  'Start Quiz',
+                  style: TextStyle(
+                    fontSize: 18,
+                  )
+                )
+              )
+          ],
+        ),
+      ),
+    );
+  }
+}
