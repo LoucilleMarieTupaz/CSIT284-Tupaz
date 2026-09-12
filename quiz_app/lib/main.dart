cecc56fb6eadaeab8e7972f494ddb7e1c2f4686b
Loucille Tupaz <loucilletupaz@gmail.com>
2026-09-13 02:42:17 +0800

    Fix main.dart

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,30 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
  
  
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-          color:Colors.deepPurple,
-          ),
-          child: Center(
-            child: Column(mainAxisAlignment:MainAxisAlignment.center,children: [
-              Image.asset('assets/logo.png',
-              width: 180,
-              ),
-              const SizedBox(height: 30), const Text('Learn Flutter the fun way!',
-                style: TextStyle(
-                fontSize: 24,fontWeight: FontWeight.bold, color:Colors.white,
-                ),
-              ),
-              const SizedBox(height:30),
-              ElevatedButton(onPressed: () {}, child: const Text('Start Quiz',style: TextStyle(fontSize: 18,)))
-            ],)
-          )
-        ),
+        body: StartScreen(),
       ),
     ),
   );
