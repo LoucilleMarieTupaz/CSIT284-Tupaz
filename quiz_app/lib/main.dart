8e2386bed17f92e2a6083c5c5bc05ef3ed8c0785
Loucille Tupaz <loucilletupaz@gmail.com>
2026-09-13 16:45:33 +0800

    Setup Questions screen and screen switch

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -1,14 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
  
  
 void main() {
-  runApp(
-    const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: StartScreen(),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ quiz_app/lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState(){
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
+
+  void switchScreen(){
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient:LinearGradient(
+              begin:Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors:[
+                Color.fromARGB(255, 135, 85, 223),
+                Colors.deepPurple,
+              ],
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
